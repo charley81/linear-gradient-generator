6fc3fe157e01a55b10af7ae1aa334a73ca731460
chrisharley81 <chrisharley81@gmail.com>
2019-06-23 13:22:45 -0400

    added comments to app.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,10 @@
+// UI Vars
 const color1 = document.getElementById('color1');
 const color2 = document.getElementById('color2');
 const text = document.getElementById('syntax');
 const bodyColor = document.getElementById('bodyColor');
 
+// getColor
 function getColor() {
   bodyColor.style.background = `linear-gradient(to right, ${color1.value}, ${
     color2.value
@@ -11,5 +13,6 @@ function getColor() {
   text.textContent = bodyColor.style.background;
 }
 
+// Event listeners
 color1.addEventListener('input', getColor);
 color2.addEventListener('input', getColor);
