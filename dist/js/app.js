c945978bc143f24544a74584b18d32d5436f8aa8
chrisharley81 <chrisharley81@gmail.com>
2019-06-23 14:49:47 -0400

    deleted page background gradient in css and added it dynamically with js

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,12 +4,26 @@ const color2 = document.getElementById('color2');
 const text = document.getElementById('syntax');
 const bodyColor = document.getElementById('bodyColor');
 
+// On DOM load
+window.addEventListener('DOMContentLoaded', () => {
+  bodyColor.style.background = `linear-gradient(to right, ${color1.value}, ${
+    color2.value
+  })`;
+
+  text.textContent = bodyColor.style.background;
+});
+
 // getColor
 function getColor() {
   bodyColor.style.background = `linear-gradient(to right, ${color1.value}, ${
     color2.value
   })`;
 
+  text.textContent = bodyColor.style.background;
+}
+
+// setText
+function setText() {
   text.textContent = bodyColor.style.background;
 }
 
