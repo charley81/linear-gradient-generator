ade1835632a7f36e6447eecd8e2b3aaf05efef2c
chrisharley81 <chrisharley81@gmail.com>
2019-06-23 17:05:37 -0400

    added random color generator button and functionality to create random linear-gradient on click

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,6 +3,7 @@ const color1 = document.getElementById('color1');
 const color2 = document.getElementById('color2');
 const text = document.getElementById('syntax');
 const bodyColor = document.getElementById('bodyColor');
+const btn = document.getElementById('btn');
 
 // On DOM load
 window.addEventListener('DOMContentLoaded', () => {
@@ -29,6 +30,18 @@ function setText() {
   text.textContent = bodyColor.style.background;
 }
 
+// randomColor
+function randomColor() {
+  const a = '#' + ((Math.random() * 0xffffff) << 0).toString(16);
+  const b = '#' + ((Math.random() * 0xffffff) << 0).toString(16);
+
+  color1.value = a;
+  color2.value = b;
+
+  bodyColor.style.background = `linear-gradient(to right, ${a}, ${b})`;
+}
+
 // Event listeners
 color1.addEventListener('input', getColor);
 color2.addEventListener('input', getColor);
+btn.addEventListener('click', randomColor);
